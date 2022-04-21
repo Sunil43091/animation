93d5db9cd3ece21516448f474dbc44abda37fdb9
Sunil43091 <102575989+Sunil43091@users.noreply.github.com>
2022-04-21 13:36:15 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,125 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "3",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+const cursor = document.querySelector('#cursor');
+let mouse = { x: 110, y: 100 };
+let pos = { x: 0, y: 0 };
+const speed = 0.1; // between 0 and 1
+
+const updatePosition = () => {
+  pos.x += (mouse.x - pos.x) * speed;
+  pos.y += (mouse.y - pos.y) * speed;
+  cursor.style.transform = 'translate3d(' + pos.x + 'px ,' + pos.y + 'px, 0)';
+};
+
+const updateCoordinates = e => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+}
+
+window.addEventListener('mousemove', updateCoordinates);
+
+function loop() {
+  updatePosition();
+  requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
+$(".toggle_Btn").click(function(){
+$(".toggle_Btn").toggleClass("navbarActive");
+});
+
+// DOM selectors
+const stars = document.getElementById('stars');
+const starsCtx = stars.getContext('2d');
+const slider = document.querySelector(".slider input");
+const output = document.querySelector("#speed");
+
+// global variables
+let screen, starsElements, starsParams = { speed: 2, number: 300, extinction: 10 };
+
+// run stars
+setupStars();
+updateStars();
+
+// handle slider
+output.innerHTML = slider.value;
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    starsParams.speed = this.value;
+};
+
+// update stars on resize to keep them centered
+window.onresize = function() {
+    setupStars();
+};
+
+// star constructor
+function Star() {
+    this.x = Math.random() * stars.width;
+    this.y = Math.random() * stars.height;
+    this.z = Math.random() * stars.width;
+
+    this.move = function() {
+        this.z -= starsParams.speed;
+        if (this.z <= 0) {
+            this.z = stars.width;
+        }
+    };
+
+    this.show = function() {
+        let x, y, rad, opacity;
+        x = (this.x - screen.c[0]) * (stars.width / this.z);
+        x = x + screen.c[0];
+        y = (this.y - screen.c[1]) * (stars.width / this.z);
+        y = y + screen.c[1];
+        rad = stars.width / this.z;
+        opacity = (rad > starsParams.extinction) ? 1.5 * (2 - rad / starsParams.extinction) : 1;
+
+        starsCtx.beginPath();
+        starsCtx.fillStyle = "#FFE55C";
+        starsCtx.arc(x, y, rad, 0, Math.PI * 2);
+        starsCtx.fill();
+    }
+}
+
+// setup <canvas>, create all the starts
+function setupStars() {
+    screen = {
+        w: window.innerWidth,
+        h: window.innerHeight,
+        c: [ window.innerWidth * 0.5, window.innerHeight * 0.5 ]
+    };
+    window.cancelAnimationFrame(updateStars);
+    stars.width = screen.w;
+    stars.height = screen.h;
+    starsElements = [];
+    for (let i = 0; i < starsParams.number; i++) {
+        starsElements[i] = new Star();
+    }
+}
+
+// redraw the frame
+function updateStars() {
+    starsCtx.fillStyle = "#000";
+    starsCtx.fillRect(0, 0, stars.width, stars.height);
+    starsElements.forEach(function (s) {
+        s.show();
+        s.move();
+    });
+    window.requestAnimationFrame(updateStars);
+}
